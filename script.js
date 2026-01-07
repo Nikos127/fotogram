5626c5f62a098c61352d21b0bb038d7c746561a4
Nikos127 <nikos127@gmail.com>
2026-01-07 14:06:11 +0100

    Try to fill

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function resp_overlay(event){
+let fotoTitles = [
+    blue - 115762_1280,
+    blue - cave - 590336_1280,
+    chair - 2736513_1280,
+    greece - 6821802_1280,
+    island - 203153_1280,
+    mykonos - 5181484_1280,
+    rhodes - 2088130_1280,
+    santorin - 1700274_1280,
+    santorin - 1788606_1280,
+    santorini - 169239_1280,
+    santorini - 86815_1280,
+    zakynthos - 1583095_1280,
+]
+
+function resp_overlay(event) {
     document.getElementById('resp_image').classList.add('overlay');
     document.getElementById('cloudy').classList.add('cloudy');
-     event.stopPropagation();
+    event.stopPropagation();
+
+    for (i = 0; i < fotoTitles.length; i++) {
+        document.getElementById('resp_image').innerHTML = ${fotoTitles[i]}
+    }
 }
 
-function overlay_close(event){
+function overlay_close(event) {
     document.getElementById('resp_image').classList.remove('overlay');
     document.getElementById('cloudy').classList.remove('cloudy');
     event.stopPropagation();
