508378a72eb8313faf383a4e41b40b4263b67aac
Nikos127 <nikos127@gmail.com>
2026-01-08 13:50:43 +0100

    index auswahl fotos

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let fotoTitles = [
-    "blue - 115762_1280",
     "blue - cave - 590336_1280",
     "chair - 2736513_1280",
     "greece - 6821802_1280",
     "island - 203153_1280",
+    "blue - 115762_1280",
     "mykonos - 5181484_1280",
     "rhodes - 2088130_1280",
     "santorin - 1700274_1280",
@@ -13,16 +13,46 @@ let fotoTitles = [
     "zakynthos - 1583095_1280"
 ]
 
-function resp_overlay(event) {
+let fotos = [
+    "./img/blue-cave-590336_1280.jpg",
+    "./img/chair-2736513_1280.jpg",
+    "./img/greece-6821802_1280.jpg",
+    "./img/island-203153_1280.jpg",
+    "./img/blue-115762_1280.jpg",
+    "./img/mykonos-5181484_1280.jpg",
+    "./img/rhodes-2088130_1280.jpg",
+    "./img/santorin-1700274_1280.jpg",
+    "./img/santorin-1788606_1280.jpg",
+    "./img/santorini-169239_1280.jpg",
+    "./img/santorini-86815_1280.jpg",
+    "./img/zakynthos-1583095_1280.jpg"
+]
+function fotosFiltered(index) {
+    resp_overlay(event, index);
+}
+
+function resp_overlay(event, index) {
     document.getElementById('resp_image').classList.add('overlay');
     document.getElementById('cloudy').classList.add('cloudy');
     event.stopPropagation();
 
-    document.getElementById('resp_image').innerHTML = ``;
-    for (i = 0; i < fotoTitles.length; i++) {
-        document.getElementById('resp_image').innerHTML += `<li>${fotoTitles[i]}</li>`
-        document.getElementById('resp_image').innerHTML += `<img src="./img/${fotoTitles[i]}.jpg" alt="">`
-    }
+    let overlayRef = document.getElementById('resp_image');
+    overlayRef.innerHTML = "";
+    let i = index;
+    overlayRef.innerHTML = getOverlayContent(i, fotoTitles, fotos);
+}
+
+
+function getOverlayContent(i) {
+    return `<div>
+            <h2>
+                ${fotoTitles[i]}
+            </h2>
+             <img class:"overlayFoto" src="./img/Close icon.png" alt="">
+        </div>
+        <div>
+            <img src="${fotos[i]}" alt=""></img>
+        </div>`
 }
 
 function overlay_close(event) {
