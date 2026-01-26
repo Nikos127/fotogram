3b09aedfab6bedc461ff37253c030f09e7c6d367
Nikos127 <119165557+Nikos127@users.noreply.github.com>
2026-01-26 13:46:15 +0100

    aria-labels

--- script.js
+++ script.js
@@ -34,26 +34,34 @@ function fotosFiltered(index) {
     respOverlay(index);
 }
 
-function nextFoto(i, l) {
-    if (i >= l - 1) {
-        i = 0;
-        respOverlay(i);
+function nextFoto(index, length) {
+    if (index >= length - 1) {
+        index = 0;
+        respOverlay(index);
     }
     else {
-        i++;
-        respOverlay(i);
+        index++;
+        respOverlay(index);
     }
+
+    setTimeout(function () {
+        document.querySelector('.overlayArrows img:last-child').focus();
+    }, 50);
 }
 
-function prewFoto(i, l) {
-    if (i <= 0) {
-        i = 11;
-        respOverlay(i);
+function prewFoto(index, length) {
+    if (index <= 0) {
+        index = length - 1;
+        respOverlay(index);
     }
     else {
-        i--;
-        respOverlay(i);
+        index--;
+        respOverlay(index);
     }
+
+    setTimeout(function () {
+        document.querySelector('.overlayArrows img:first-child').focus();
+    }, 50);
 }
 
 function respOverlay(index) {
@@ -62,6 +70,10 @@ function respOverlay(index) {
     document.getElementById('overflow').classList.add('overlay');
     event.stopPropagation();
 
+    let photoTabs = document.querySelectorAll('main button');
+    for (let i = 0; i < photoTabs.length; i++) {
+        photoTabs[i].tabIndex = -1;
+    }
 
     let overlayRef = document.getElementById('respImage');
     overlayRef.innerHTML = "";
@@ -73,23 +85,21 @@ function respOverlay(index) {
 
 
 function getOverlayContent(index, length) {
-    return `<div>
-            <div class="overlayHeader">
+    return `<div class="overlayHeader">
                 <h2>
                     ${fotoTitles[index]}
                 </h2>
                 <div class="overlayClose">
-                    <img onclick="overlayClose()" src="./img/Close icon.png" alt=""></img>
+                    <img aria-label="Schließen" aria-describedby="Schließt das Overlay" onclick="overlayClose()" onkeydown="if(event.key==='Enter') overlayClose()" src="./img/Close icon.png" alt="" tabindex="0"></img>
                 </div>
             </div>
             <div class="overlayPhoto">
                 <img src="${fotos[index]}" alt=""></img>
             </div>
             <div class="overlayArrows">
-                <img onclick="prewFoto(${index},${length})" src="./img/Button left.png" alt="arrowPointLeft">
+                <img onclick="prewFoto(${index},${length})" onkeydown="if(event.key==='Enter') prewFoto(${index},${length})" src="./img/Button left.png" alt="arrowPointLeft" tabindex="0">
                 <p class="counter">${index + 1}/${length}</p>
-                <img onclick="nextFoto(${index},${length})" src="./img/Button right.png" alt="arrowPointRight">
-            </div>
+                <img onclick="nextFoto(${index},${length})" onkeydown="if(event.key==='Enter') nextFoto(${index},${length})" src="./img/Button right.png" alt="arrowPointRight" tabindex="0">
         </div>`
 }
 
@@ -97,6 +107,11 @@ function overlayClose() {
     document.getElementById('respImage').classList.remove('overlay');
     document.getElementById('cloudy').classList.remove('cloudy');
     document.getElementById('overflow').classList.remove('overlay');
+
+    let photoTabs = document.querySelectorAll('main button');
+    for (let i = 0; i < photoTabs.length; i++) {
+        photoTabs[i].tabIndex = 0;
+    }
 }
 
 document.addEventListener('keydown', function (x) {
